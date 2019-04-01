4ba3488ebe336f312f8bda7c009737ec605c8dbe
ErbyAusten <47074392+ErbyAusten@users.noreply.github.com>
2019-03-31 23:07:04 -0400

    Add files via upload

---
+++ DestroyByContact.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private Game_Controller game_Controller;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            game_Controller = gameControllerObject.GetComponent<Game_Controller>();
+        }
+        if (game_Controller == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            game_Controller.GameOver();
+        }
+        game_Controller.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
